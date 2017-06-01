d53dc93dbdafb25e63331386046c62688b884a30
benweizhu <benweizhu@gmail.com>
2017-06-01 14:58:04 +0800

    init

---
+++ component-functional-pure-component/app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloMessage extends React.Component {
+    render() {
+        return <div>Hello, {this.props.message}</div>;
+    }
+}
+
+ReactDOM.render(<HelloMessage message="My First React App" />, document.getElementById('app'));
